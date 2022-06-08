b70a13b287e7d64b1fd358c3901883c76154c100
11081999 <robertoriverateran@gmail.com>
2022-06-08 16:20:42 -0500

    Update GD.py

--- Complementary_Classes/GoogleMLCrashCourse/Programs/Gradient Descent/GD.py
+++ Complementary_Classes/GoogleMLCrashCourse/Programs/Gradient Descent/GD.py
@@ -1,7 +1,6 @@
 #https://www.youtube.com/watch?v=vsWrXfO3wWw
 
 #! OBJECTIVE: This program aims to study the concepts of Gradient descent
-
 #! Additionally, we´ll get the m and b values.
 
 #by Roberto Rivera Terán
@@ -13,7 +12,48 @@ import matplotlib.pyplot as plt
 
 #Step 1: Make the Gradient Descent algorithm
 def gradient_descent(x, y):
-    m_curr, b_curr = 0 
+    #? remember the formula y' = m*x + b
+    m_curr = b_curr = 0
+    iterarions = 1000
+    n= len(x)
+
+    #Trial and error
+    learing_rate= 0.08
+
+    iter= 0
+    for i in range(iterarions):
+
+        Y_pred = m_curr*x + b_curr
+
+        cost= (1/n) * sum([val**2 for val in (y-Y_pred)])
+
+        # m¨s derivative 
+        md= -(2 / n) * sum(x * (y - Y_pred))
+        # b¨s derivative 
+        bd= -(2 / n) * sum(y - Y_pred)
+
+        # Apply slight variation to the m and b values.
+        m_curr = m_curr - learing_rate * md
+        b_curr = b_curr - learing_rate * bd
+
+        #Visulize the adjustment
+        plt.plot(x , Y_pred, color = 'red')
+
+        iter= i
+
+    print("\n____________________________________")
+    print("\n >> Gradient Descent: \n")
+    print("m: {} \nb: {} \nCost: {} \niteration: {} \n".format(
+        m_curr, b_curr, cost, iter))
+
+    #?  The graph seems kinds wrong though 
+    #?  I believe there should not be any more values above 13
+    #?  Since the furthest value of X is 5 
+
+    #Visualize the final adjustment (the most accurate one for predictions)
+    plt.plot(x , Y_pred, color = 'blue')
+    plt.show()
+
 
 #Step 2: Import dataset.
 dataset = pd.read_csv(r'C:\Users\Roberto\Desktop\100DaysOfMLCode\datasets\studentscores.csv')
@@ -24,20 +64,17 @@ X = np.array(dataset.iloc[ : ,   : 1 ].values)
 # Y: Target (single dim array)
 Y = np.array(dataset.iloc[ : , 1 ].values)
 
+#Alternative data, Expected m= 2, b= 3
+# X: data
+X = np.array([1,2,3,4,5])
+# Y: Target (single dim array)
+Y = np.array([5,7,9,11,13])
+
 print("\n____________________________________")
 print("\n >> Data: \n")
 print("X : \n", X)
 print("Y: \n", Y)
 
-#Step 2: 
-
-#?  In order to do the "Hot and cold" iterative loss approach-
-#?  We would have needed to use the formula y'= b + w1*x1 ...
-#?  so that we can have some sort of randomized guesses- 
-#?  that would end up minimizing the loss.
-#?  we could maybe use an algorithm (https://www.geeksforgeeks.org/linear-regression-python-implementation/)
-#?  so that we can pick random values for b and w1 
-
-
+gradient_descent(X, Y)
 
 #?: Done !
